ae5a9c064fd3ff630d06311e4277bb9d86dbe9fd
sanalskumar <sanalskumar126@gmail.com>
2024-07-11 16:59:42 +0530

    setup finished

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 import connectDB from './db.js';
 import { config } from './config/config.js';
+import dotenv from 'dotenv';
+import { socketController} from './controllers/socketController.js';
+
+
 
 const app = express();
 
 const server = http.createServer(app);
+
+// Load environment variables from .env file
+dotenv.config();
+
 
 const io = new Server(server,{
     cors:{
@@ -20,6 +28,9 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+io.on('connection', socketController.js);
+
 
 server.listen(config.PORT || 3000, () => {
     console.log(`Server is running on port ${config.PORT}`);
