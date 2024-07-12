a18a16537aca36be8dbd2e241598b0cea016b0c2
sanalskumar <sanalskumar126@gmail.com>
2024-07-12 16:07:24 +0530

    frontend configuration done

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='text-center'>
+      <h2 className='text-red-400'>
+        hello
+      </h2>
+    </div>
+  )
+}
+
+export default App
